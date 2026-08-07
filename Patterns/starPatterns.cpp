58fe1247d66d2ec94a8286e86b71aadabdaead6b
rehan11m4 <rehan.s@communicationcrafts.com>
2026-08-07 22:43:09 +0530

    Add various star pattern functions in C++

--- Patterns/starPatterns.cpp
+++ Patterns/starPatterns.cpp
@@ -1,6 +1,119 @@
 #include<iostream>
 using namespace std;
 
+void squareStar(int n){
+    for(int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void triangleStar(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void reverseTriangle(int n){
+    for (int i = n; i > 0; i--){
+        for (int j = 0; j < i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pyramidStar(int n){
+    for (int i = 0; i < n; i++){
+        for (int k = 0; k < n - i - 1; k++){
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void invertedPyramidStar(int n){
+    for (int i = n; i > 0; i--){
+        for (int k = 0; k <= n - i - 1; k++){
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void diamondStar(int n){
+    for (int i = 0; i < n; i++){
+        for (int k = 0; k < n - i - 1; k++){
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = n-1; i > 0; i--){
+        for (int k = 0; k <= n - i - 1; k++){
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void halfDiamondStar(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for (int i = n - 1; i > 0; i--){
+        for (int j = 0; j < i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void hollowSquareStar(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if(i == 0 || i == n - 1 || j == 0 || j == n - 1){
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void symetricVoidPattern(int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if(i == j || i + j == n - 1){
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
+
 void butterflyPattern(int n){
     for(int i = 0; i < n; i++){
         for(int j = 0; j < i; j++){
@@ -56,9 +169,27 @@ void hollowDiamond(int n){
 }
 
 int main(){
-    int n = 10;
-    butterflyPattern(n);
+    int num = 10;
+    butterflyPattern(num);
     cout << endl;
-    hollowDiamond(n);
+    hollowDiamond(num);
+    cout << endl;
+    squareStar(num);
+    cout << endl;
+    triangleStar(num);
+    cout << endl;
+    reverseTriangle(num);
+    cout << endl;
+    pyramidStar(num);
+    cout << endl;
+    invertedPyramidStar(num);
+    cout << endl;
+    diamondStar(num);
+    cout << endl;
+    halfDiamondStar(num);
+    cout << endl;
+    hollowSquareStar(num);
+    cout << endl;
+    symetricVoidPattern(num);
     return 0;
 }
