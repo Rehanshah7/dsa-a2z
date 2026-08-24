508c1d61d821301f58db1889a25ae7cd0a4be475
rehan11m4 <rehan.s@communicationcrafts.com>
2026-08-24 13:31:33 +0530

    Add linear search function and improve longest consecutive length algorithms

--- arrayProblems/longestConsecutiveLength.cpp
+++ arrayProblems/longestConsecutiveLength.cpp
@@ -3,25 +3,83 @@ using namespace std;
 
 class Solution{
     public:
-    int longestConsecutiveLength(int arr[], int n){
-        int length = 0;
+    // function to check if an element is present in an array
+    int linearSearch(int arr[], int n, int target){
+        for(int i = 0; i < n; i++){
+            if(arr[i] == target){
+                return true;;
+            }
+        }
 
-        // pick each element and find its consecutive length
-        for (int i = 0; i < n; i++){
+        return false;
+    }
+    int longestConsecutiveLength(int arr[], int n){
+        int length = 1;
+
+        // check for each element
+        for(int i = 0; i < n; i++){
+            // check if the current element is the start of a consecutive sequence and update its count default to 1
             int current = arr[i];
             int count = 1;
-
-            // search the next consecutive elements
-            for (int j = 0; j < n; j++){
-                if (arr[j] == current + 1){
-                    count++;
-                    current = arr[j];
-                }
+            // check if the next element is present in the array
+            while(linearSearch(arr, n, current + 1) == true){
+                count++;
+                current++;
             }
 
-            // update the maximum length
+            // update the length
             length = max(length, count);
         }
+
+        return length;
+    }
+
+    int longestConsectiveLengthBetterApproach(int arr[], int n){
+        sort(arr, arr + n);
+        int length = 1;
+        int lastSmallerElement = arr[0];
+        int count = 1;
+        for(int i = 1; i < n; i++){
+            if(arr[i] - 1 == lastSmallerElement){
+                count++;
+                lastSmallerElement = arr[i];
+            } else if(arr[i] != lastSmallerElement){
+                count = 1;
+                lastSmallerElement = arr[i];
+            }
+
+            length = max(length, count);
+        }
+
+        return length;
+    }
+
+    int longestConsecutiveLengthOptimalApproach(int arr[], int n){
+        int length = 1;
+        int count = 0;
+        int current = arr[0];
+        unordered_set<int> st;
+        for(int i = 0; i < n; i++){
+            st.insert(arr[i]);
+        }
+
+        for(auto it: st){
+            cout << it << " ";
+        }
+
+        for(auto it: st){
+            if(st.find(it - 1) == st.end()){
+                current = it;
+                count = 1;
+                while(st.find(current + 1) != st.end()){
+                    count++;
+                    current++;
+                }
+                length = max(length, count);
+            }
+        }
+        cout << endl;
+
         return length;
     }
 };
@@ -40,6 +98,10 @@ int main(){
 
     Solution obj;
     int count = obj.longestConsecutiveLength(arr, n);
+    int countBetter = obj.longestConsectiveLengthBetterApproach(arr, n);
+    int countOptimal = obj.longestConsecutiveLengthOptimalApproach(arr, n);
     cout << "The longest consecutive length is: " << count << endl;
+    cout << "The longest consecutive length in better approach is: " << countBetter << endl;
+    cout << "The longest consecutive length in optimal approach is: " << countOptimal << endl;
     return 0;
 }
